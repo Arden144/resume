b8bd07f963b758d64455e52de1591d5e18fbcbf8
Arden Sinclair <general@ardensinclair.com>
2023-10-02 01:11:08 -0700

    Add commit timestamp to PDFs

--- scripts/pdf.ts
+++ scripts/pdf.ts
@@ -14,11 +14,38 @@ const generatePdf = async (browser: Browser, url: string): Promise<Buffer> => {
     return pdf;
 };
 
+const parseArgs = (arglist: string[]): [string[], Record<string, string | boolean>] => {
+    const [args, kwargs, name] = arglist.reduce<[string[], Record<string, string | boolean>, string | undefined]>(([args, kwargs, name], arg) => {
+        if (arg.startsWith("--")) {
+            return [args, { ...kwargs, ...(name && { [name]: true }) }, arg.slice(2)];
+        } else if (arg.startsWith("-")) {
+            return [args, { ...kwargs, ...(name && { [name]: true }) }, arg.slice(1)];
+        } else {
+            if (name !== undefined) {
+                return [args, { ...kwargs, [name]: arg }, undefined];
+            } else {
+                return [[...args, arg], kwargs, undefined];
+            }
+        }
+    }, [[], {}, undefined]);
+    return [args, { ...kwargs, ...(name && { [name]: true }) }];
+}
+
 const main = async () => {
+    const [_args, kwargs] = parseArgs(process.argv.slice(2));
+    const timestamp = typeof kwargs.timestamp === "string" ? new Date(kwargs.timestamp) : new Date();
+    const year = timestamp.getFullYear().toString();
+    const month = timestamp.getMonth().toString().padStart(2, "0");
+    const date = timestamp.getDate().toString().padStart(2, "0");
+
     const baseDir = new URL('..', import.meta.url);
 
     const config = {
-        root: fileURLToPath(baseDir)
+        root: fileURLToPath(baseDir),
+        logLevel: "error",
+        vite: {
+            logLevel: "error"
+        }
     } satisfies AstroInlineConfig;
 
     const browser = launch({ headless: "new" });
@@ -28,8 +55,11 @@ const main = async () => {
 
     const pdf = await generatePdf(await browser, `http://${server.host}:${server.port}`);
 
+    const output = new URL(`./Arden-Sinclair-Resume-${year}-${month}-${date}.pdf`, baseDir);
+    console.log(fileURLToPath(output));
+
     await Promise.all([
-        write(new URL("./resume.pdf", baseDir), pdf),
+        write(output, pdf),
         server.stop(),
         (await browser).close()
     ]);
